52ede1d1de044bf75d3777cae7e71cc1f4409547
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-08-25 15:19:43 -0400

    the game is working need to make it cleaner and personalized

--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from './components/button.js';
 import Card from './components/Card.js';
-import cardData from './data/card-data.json';
-import Navbar from './components/Navbar.js';
+// import Navbar from './components/Navbar.js';
+
+const cardImages = [
+  { imgSrc: 'images/beatles.jpeg', matched: false },
+  { imgSrc: 'images/blink182.jpeg', matched: false },
+  { imgSrc: 'images/fkatwigs.jpeg', matched: false },
+  { imgSrc: 'images/fleetwood.jpeg', matched: false },
+  { imgSrc: 'images/joy-division.jpeg', matched: false },
+  { imgSrc: 'images/ledzep.jpeg', matched: false },
+  { imgSrc: 'images/metallica.jpeg', matched: false },
+  { imgSrc: 'images/pinkfloyd.jpeg', matched: false },
+];
 
 const App = () => {
-  const [playersLivesCount, setPlayersLivesCount] = useState(6);
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
-  function random() {
+  const randomCompare = () => {
     return Math.random() - 0.5;
-  }
-  const randomCards = cardData.sort(() => random());
+  };
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => randomCompare())
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // Handle choice
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.imgSrc === choiceTwo.imgSrc) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.imgSrc === choiceOne.imgSrc) {
+              return {
+                ...card,
+                matched: true,
+              };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    console.log('reset');
+  };
 
   return (
     <div className='container'>
-      {/* <Navbar />
+      {/* <Navbar /> */}
       <header>
-        <h1>
-          Lives: <span className='playerLivesCount'>{playersLivesCount}</span>
-        </h1>
-      </header> */}
-      <div className='game'>
+        <h1>Magic Match</h1>
+        <Button onClick={shuffleCards} text='New Game' />|
+        <span className='playerLivesCount'>{turns}</span>
+      </header>
+      <div className='card-grid'>
         <section>
-          {randomCards.map((card, index) => {
-            return <Card key={index} card={card} />;
+          {cards.map((card) => {
+            return (
+              <Card
+                key={card.id}
+                card={card}
+                handleChoice={handleChoice}
+                flipped={
+                  card === choiceOne || card === choiceTwo || card.matched
+                }
+              />
+            );
           })}
         </section>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const Card = ({ card }) => {
+const Card = ({ card, handleChoice, flipped }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
   return (
-    <div className='card' name={card.name} id={card.id}>
-      <img src={card.imgSrc} alt='card' className='face' />
-      <div className='back'></div>
+    <div className='card'>
+      <div className={flipped ? 'flipped' : ''}>
+        <img src={card.imgSrc} alt='card front' className='front' />
+        <img
+          src='/images/lamp.jpg'
+          onClick={handleClick}
+          alt='card back'
+          className='back'
+        />
+      </div>
     </div>
   );
 };
